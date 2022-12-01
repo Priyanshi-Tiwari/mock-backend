d40a20618f39d1dd2a4d2dd462863185705818fd
Rahul Vijaykumar Dhadge <rahuldhadge@Rahuls-MacBook-Pro.local>
2022-12-01 22:16:54 +0530

    Update route added to client account API

--- models/clientAccounts.model.js
+++ models/clientAccounts.model.js
@@ -3,12 +3,19 @@ const clientAccountsSchema = mongoose.Schema({
     name: {
         type: String,
         trim: true,
+        unique: true,
         required: true,
     },
     account_manager_name: {
         type: String,
         trim: true,
         required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["Active", "Archived", "Planned"],
+        default: "Active",
     },
 }, {
     timestamps: true,
